0e941822b73cf6ec1f055417b9b3cb7121d47f58
facutise <facundo.tisera123@gmail.com>
2024-06-11 23:06:08 -0300

    query en X y Y, QueryHunter script custom para prueba Spatial GrId FUNCIONA SI LOS BOIDS SON HIJO

--- Assets/ClaseGrid/Grid/Queries.cs
+++ Assets/ClaseGrid/Grid/Queries.cs
@@ -22,6 +22,71 @@ public class Queries : MonoBehaviour
             //posición final --> esquina inferior derecha de la "caja"
             //como funcion para filtrar le damos una que siempre devuelve true, para que no filtre nada.
             return targetGrid.Query(
+                transform.position + new Vector3(-w, -h, 0),
+                transform.position + new Vector3(w, h,0),
+                x => true);
+        }
+        else
+        {
+            //creo una "caja" con las dimensiones deseadas, y luego filtro segun distancia para formar el círculo
+            return targetGrid.Query(
+                transform.position + new Vector3(-radius, -radius, 0),
+                transform.position + new Vector3(radius, radius, 0),
+                x => {
+                    var position2d = x - transform.position;
+                    position2d.z = 0;
+                    return position2d.sqrMagnitude < radius * radius;
+                });
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (targetGrid == null)
+            return;
+
+        //Flatten the sphere we're going to draw
+        Gizmos.color = Color.cyan;
+        if (isBox)
+            Gizmos.DrawWireCube(transform.position, new Vector3(width, height,0));
+        else
+        {
+            Gizmos.matrix *= Matrix4x4.Scale(Vector3.forward + Vector3.right);
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+
+        if (Application.isPlaying)
+        {
+            selected = Query();
+            var temp = FindObjectsOfType<GridEntity>().Where(x=>!selected.Contains(x));
+            foreach (var item in temp)
+            {
+                item.onGrid = false;
+            }
+            foreach (var item in selected)
+            {
+                item.onGrid = true;
+            }
+
+        }
+    }
+
+
+    private void OnGUI()
+    {
+        GUI.Label( new Rect(0,0,20,20), "HOLA");
+    }
+    /*
+      public IEnumerable<GridEntity> Query()
+    {
+        if (isBox)
+        {
+            var h = height * 0.5f;
+            var w = width * 0.5f;
+            //posicion inicial --> esquina superior izquierda de la "caja"
+            //posición final --> esquina inferior derecha de la "caja"
+            //como funcion para filtrar le damos una que siempre devuelve true, para que no filtre nada.
+            return targetGrid.Query(
                 transform.position + new Vector3(-w, 0, -h),
                 transform.position + new Vector3(w, 0, h),
                 x => true);
@@ -76,4 +141,6 @@ public class Queries : MonoBehaviour
     {
         GUI.Label( new Rect(0,0,20,20), "HOLA");
     }
+     
+     */
 }
